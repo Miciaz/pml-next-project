d70f0c6036f25f9214b992410f83e36f8c6f9d23
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2024-07-26 11:56:49 +0200

    Eliminata struttura progetto iniziale

--- pml-webapp/app/page.tsx
+++ pml-webapp/app/page.tsx
@@ -1,13 +1,11 @@
 // app/page.tsx
 import React from 'react';
-import Carousel from './components/Carousel';
-import MatchSchedule from './components/MatchSchedule';
+
 
 export default function Home() {
   return (
     <main>
-      <Carousel />
-      <MatchSchedule />
+      
     </main>
   );
 }
